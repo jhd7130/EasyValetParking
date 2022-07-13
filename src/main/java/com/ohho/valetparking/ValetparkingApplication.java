cc8f385c9d6ea380d88533241446690637b5b59b
jhd7130 <jhd7130@github.com>
2022-07-13 23:13:30 +0900

    first project  enviroment

---
+++ src/main/java/com/ohho/valetparking/ValetparkingApplication.java
@@ -0,0 +1,13 @@
+package com.ohho.valetparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValetparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValetparkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohho/valetparking/ValetparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohho.valetparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValetparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
