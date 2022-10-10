1d648458c21ff86348f1dcd46ded4421dfb0b20f
jhd7130 <jhd7130@github.com>
2022-10-10 16:29:00 +0900

    Fix/Exception, Fix UnExpectiedException

--- src/main/java/com/ohho/valetparking/global/error/exception/UnExpectedException.java
+++ src/main/java/com/ohho/valetparking/global/error/exception/UnExpectedException.java
@@ -5,9 +5,7 @@ package com.ohho.valetparking.global.error.exception;
  **/
 public class UnExpectedException extends Exception {
   public UnExpectedException(){}
-  public UnExpectedException(String message){
-    super("예상하지 못한 에러입니다.");
-  }
   public UnExpectedException(Exception e) {
+    super("예상하지 못한 에러입니다.");
   }
 }
