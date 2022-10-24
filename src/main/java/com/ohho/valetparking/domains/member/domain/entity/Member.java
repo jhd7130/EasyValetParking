cf8435e1c093c88052aea98e0b9522baa4409c9f
jhd7130 <jhd7130@github.com>
2022-10-24 15:37:08 +0900

    INFRA/DOCKER ADD Dockerfile

--- src/main/java/com/ohho/valetparking/domains/member/domain/entity/Member.java
+++ src/main/java/com/ohho/valetparking/domains/member/domain/entity/Member.java
@@ -1,16 +1,10 @@
 package com.ohho.valetparking.domains.member.domain.entity;
 
-import com.ohho.valetparking.domains.member.domain.dto.SignInResponse;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
-/**
- * Role :
- * Responsibility :
- * Cooperation with :
- **/
 @AllArgsConstructor
 @ToString
 @Getter
